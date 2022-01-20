e6e85a5e2c6cc98329aee55e2d205eaf639ccd21
chrisbyd <chenyongbiao0319@sina.com>
2022-01-20 15:21:10 +0800

    added contrastive loss

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,12 +1,14 @@
 import torch.nn.functional as F
 
 from .triplet_loss import TripletLoss, CrossEntropyLabelSmooth
+from .contrast_loss import SupConLoss
 from .orthogonal_loss import OrthogonalLoss
 
 def make_loss(cfg, num_classes):
     sampler = cfg.DATALOADER.SAMPLER
     triplet = TripletLoss(cfg.SOLVER.MARGIN)
     orthogonal = OrthogonalLoss()
+    supcon = SupConLoss()
 
     if cfg.MODEL.LABEL_SMOOTH == 'on':
         xent = CrossEntropyLabelSmooth(num_classes=num_classes)
@@ -22,7 +24,10 @@ def make_loss(cfg, num_classes):
             return triplet(feat, target)[0]
     elif sampler == 'softmax_triplet':
         def loss_func(score, feat, descriptor, target):
-            return xent(score, target) + triplet(feat, descriptor ,target)[0] 
+            return xent(score, target) + triplet(feat, descriptor ,target)[0]
+    elif sampler == 'softmax_contrast':
+        def loss_func(score, feat, descriptor, target):
+            return xent(score, target) + supcon(feat, target, descriptor, target) 
     else:
         print('expected sampler should be softmax, triplet or softmax_triplet, '
               'but got {}'.format(cfg.DATALOADER.SAMPLER))
