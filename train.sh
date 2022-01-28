bf471988868cf0cb9713002dd1d6726272ecce7f
chrisbyd <chenyongbiao0319@sina.com>
2022-01-28 18:05:45 +0800

    all full model

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # CUDA_VISIBLE_DEVICES=1 python main.py -c configs/veri_1024.yml
 
-# CUDA_VISIBLE_DEVICES=1 python main.py -c configs/veri_512.yml
+CUDA_VISIBLE_DEVICES=1 python main.py -c configs/veri_512.yml
 
 CUDA_VISIBLE_DEVICES=1 python main.py -c configs/veri_256.yml
 
-CUDA_VISIBLE_DEVICES=1 python main.py -c configs/veri_512.yml
+CUDA_VISIBLE_DEVICES=1 python main.py -c configs/veri_2048.yml
---
+++ train_vid.sh
@@ -0,0 +1,4 @@
+# CUDA_VISIBLE_DEVICES=0 python main.py -c configs/vehicleid_256.yml
+# CUDA_VISIBLE_DEVICES=0 python main.py -c configs/vehicleid_512.yml
+# CUDA_VISIBLE_DEVICES=0 python main.py -c configs/vehicleid_1024.yml
+CUDA_VISIBLE_DEVICES=0 python main.py -c configs/vehicleid_2048.yml
